8d186251ee3234debfdcf4284ddc72ed91f4f1b7
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-07-17 16:42:21 +0300

    create vpc.tf and variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "AWS_Region" {
+    default = "eu-central-1"
+}
+
+variable "instance_type" {
+    default = "t4g.micro"
+}
+
+variable "vpc_cidr_block" {
+    default = "10.60.0.0/16"
+}
+
+variable "availability_zones" {
+    default = {
+        eu-central-1a = 1
+        eu-central-1b = 2
+        eu-central-1c = 3
+    }
+}
+
+variable "web_key_name" {
+    default = "key123"
+}
