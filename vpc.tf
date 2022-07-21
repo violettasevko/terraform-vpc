d4f0afdf7046c3d7ceacabd4010b772da43b2162
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-07-21 15:41:39 +0300

    minus each value

--- vpc.tf
+++ vpc.tf
@@ -17,8 +17,8 @@ resource "aws_subnet" "pub_subnet" {
     vpc_id = "${aws_vpc.myvpc22.id}"
 
     availability_zone = var.az
-    cidr_block        = cidrsubnet(aws_vpc.myvpc22.cidr_block, 8, each.value)
-    ipv6_cidr_block   = cidrsubnet(aws_vpc.myvpc22.ipv6_cidr_block, 8, each.value + 6)
+    cidr_block        = cidrsubnet(aws_vpc.myvpc22.cidr_block, 8, 1)
+    ipv6_cidr_block   = cidrsubnet(aws_vpc.myvpc22.ipv6_cidr_block, 8, 6)
  
     map_public_ip_on_launch = "true" //it makes this a public subnet
     assign_ipv6_address_on_creation = true
@@ -33,8 +33,8 @@ resource "aws_subnet" "priv_subnet" {
     vpc_id = "${aws_vpc.myvpc22.id}"
 
     availability_zone = var.az
-    cidr_block        = cidrsubnet(aws_vpc.myvpc22.cidr_block, 8, each.value + 6)
-    ipv6_cidr_block        = cidrsubnet(aws_vpc.myvpc22.ipv6_cidr_block, 8, each.value + 22)
+    cidr_block        = cidrsubnet(aws_vpc.myvpc22.cidr_block, 8, 6)
+    ipv6_cidr_block        = cidrsubnet(aws_vpc.myvpc22.ipv6_cidr_block, 8, 22)
 
     map_public_ip_on_launch = "true" //it makes this a private subnet
     assign_ipv6_address_on_creation = false
