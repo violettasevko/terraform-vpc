62121f08d3af2ac443d8f758602d5199b5ff1e1d
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-07-25 17:54:56 +0300

    paris

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "AWS_Region" {
-    default = "us-east-1"
+    default = "eu-west-3"
 }
 
 variable "vpc_cidr_block" {
-    default = "10.60.0.0/16"
+    default = "10.63.0.0/16"
 }
 
 variable "az" {
-    default = "us-east-1a"
+    default = "eu-west-3a"
 }
