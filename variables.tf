120c9ea07ed844605e3f1efaae2b5531cea2acfd
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-07-25 17:25:09 +0300

    delete useless lines

--- variables.tf
+++ variables.tf
@@ -6,14 +6,6 @@ variable "vpc_cidr_block" {
     default = "10.61.0.0/16"
 }
 
-variable "instance_type" {
-    default = "t4g.micro"
-}
-
 variable "az" {
     default = "eu-central-1a"
-}
-
-variable "web_key_name" {
-    default = "lunakey"
 }
