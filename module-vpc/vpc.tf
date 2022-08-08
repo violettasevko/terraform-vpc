a0eab82b2fb03b9a746bb5b68d296868e60e4154
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-08-08 22:08:55 +0300

    a

--- module-vpc/vpc.tf
+++ module-vpc/vpc.tf
@@ -44,20 +44,21 @@ module "vpc" {
   public_subnet_ipv6_prefixes = [11, 12, 13]
   private_subnet_ipv6_prefixes = [21, 22, 23]
 
-private_subnet_tags = {
-  Name = {
-  "${var.AWS_Region}a" = "Private subnet A"
-  "${var.AWS_Region}b" = "Private subnet B"
-  "${var.AWS_Region}c" = "Private subnet C"
+  private_subnet_tags = {
+     type = "map"
+    subnet[0] = { "Name" = "Private subnet A"}
+  subnet[1] = { "Name" = "Private subnet B"}
+  subnet[2] = { "Name" = "Private subnet C"}
   }
-}
 
 public_subnet_tags = {
-  Name = {
-    "${var.AWS_Region}a" = "Public subnet A"
-    "${var.AWS_Region}b" = "Public subnet B"
-    "${var.AWS_Region}c" = "Public subnet C"
-  }
+  type = "map"
+
+  subnet[0] = { "Name" = "Public subnet A"}
+    subnet[1] = { "Name" = "Public subnet B"
+    }
+    subnet[2] = { "Name" = "Public subnet C"
+}
 }
 
   tags = {
