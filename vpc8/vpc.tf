8f24ce8588a9fb54e637bdfed43f7f79501fbae1
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-08-13 23:24:45 +0300

    create vpc8

---
+++ vpc8/vpc.tf
@@ -0,0 +1,47 @@
+variable "AWS_Region" {
+  description = "type a region (default - eu-central-1)"
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "vpc_prefix" {
+  description = "type a cidr (default - 10.78)"
+  type    = string
+  default = "10.78"
+}
+
+#sample
+#terraform apply -var="AWS_Region=eu-central-1" -var="vpc_prefix=10.61"
+
+provider "aws" {
+    region = var.AWS_Region
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "Vpc8"
+  cidr = "${var.vpc_prefix}.0.0/16"
+
+  azs             = ["${var.AWS_Region}a", "${var.AWS_Region}b", "${var.AWS_Region}c"]
+   private_subnets = ["${var.vpc_prefix}.21.0/24", "${var.vpc_prefix}.22.0/24", "${var.vpc_prefix}.23.0/24"]
+   
+  enable_nat_gateway = false
+  
+  default_route_table_name = "igw8"
+
+  enable_ipv6 = true
+  assign_ipv6_address_on_creation = true
+
+  private_subnet_ipv6_prefixes = [33, 34, 35]
+
+private_subnet_tags = {
+  Name = "Vpc4 private subnet A"
+  Name = "Vpc4 private subnet B"
+  Name = "Vpc4 private subnet C"
+}
+
+  tags = {
+    owner = "violetta"
+  }
+}
