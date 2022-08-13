12ece352196d028c6057a53a4978c1be81ca09db
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-08-13 23:14:18 +0300

    vpc6

---
+++ vpc6/vpc.tf
@@ -0,0 +1,58 @@
+variable "AWS_Region" {
+  description = "type a region (default - eu-central-1)"
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "vpc_prefix" {
+  description = "type a cidr (default - 10.73)"
+  type    = string
+  default = "10.73"
+}
+
+#sample
+#terraform apply -var="AWS_Region=eu-central-1" -var="vpc_prefix=10.61"
+
+provider "aws" {
+    region = var.AWS_Region
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "Vpc3"
+  cidr = "${var.vpc_prefix}.0.0/16"
+
+  azs             = ["${var.AWS_Region}a", "${var.AWS_Region}b", "${var.AWS_Region}c"]
+   public_subnets  = ["${var.vpc_prefix}.11.0/24", "${var.vpc_prefix}.12.0/24", "${var.vpc_prefix}.13.0/24"]
+   intra_subnets = ["${var.vpc_prefix}.31.0/24", "${var.vpc_prefix}.32.0/24", "${var.vpc_prefix}.33.0/24"]
+  
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = false
+  
+  default_route_table_name = "igw3"
+
+  enable_ipv6 = true
+  assign_ipv6_address_on_creation = true
+
+  public_subnet_ipv6_prefixes = [17, 18, 19]
+  intra_subnet_ipv6_prefixes = [51, 52, 53]
+
+public_subnet_tags = {
+  Name = "Vpc3 public subnet A"
+  Name = "Vpc3 public subnet B"
+  Name = "Vpc3 public subnet C"
+}
+
+intra_subnet_tags = {
+  Name = "Vpc3 intra subnet A"
+  Name = "Vpc3 intra subnet B"
+  Name = "Vpc3 intra subnet C"
+}
+
+  tags = {
+    owner = "violetta"
+  }
+}
+
