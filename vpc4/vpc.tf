ae3608c0eb4959113cb92b2a1f81f5fc4927f913
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-08-24 18:01:37 +0300

    Update vpc.tf

--- vpc4/vpc.tf
+++ vpc4/vpc.tf
@@ -5,9 +5,9 @@ variable "AWS_Region" {
 }
 
 variable "vpc_prefix" {
-  description = "type a cidr (default - 10.74)"
+  description = "type a cidr (default - 10.50)"
   type    = string
-  default = "10.74"
+  default = "10.50"
 }
 
 #sample
